ddd51006eaa03225b76d75a8b6c1ae2e3245fabc
Yothin_Error <yothin_theone@hotmail.com>
2017-02-05 02:39:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/yothin_error/uploads/Constants.java
@@ -0,0 +1,10 @@
+package com.example.yothin_error.uploads;
+
+/**
+ * Created by Yothin_Error on 1/2/2560.
+ */
+
+public class Constants {
+    public static final String UPLOAD_URL = "http://172.20.10.9/test/upload.php";
+    public static final String IMAGES_URL = "http://172.20.10.9/test/getImages.php";
+}
---
+++ app/src/main/java/com/example/yothin_error/uploads/MainActivity.java
@@ -0,0 +1,219 @@
+package com.example.yothin_error.uploads;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.kosalgeek.android.photoutil.CameraPhoto;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private Button btnCamera;
+    private ImageView imageView;
+    private EditText editText;
+    CameraPhoto cameraPhoto;
+    private int PICK_IMAGE_REQUEST = 1;
+    private int REQUEST_CAMERA = 2;
+
+
+    //storage permission code
+    private static final int STORAGE_PERMISSION_CODE = 123;
+
+    //Bitmap to get image from gallery
+    private Bitmap bitmap;
+
+    //Uri to store the image uri
+    private Uri filePath;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestStoragePermission();
+        cameraPhoto = new CameraPhoto(getApplicationContext());
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+        cameraPhoto = new CameraPhoto(getApplicationContext());
+        imageView = (ImageView) findViewById(R.id.imageView);
+        editText = (EditText) findViewById(R.id.editTextName);
+
+        //Setting clicklistener
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+        btnCamera.setOnClickListener(this);
+    }
+
+    public void uploadMultipart() {
+        //getting name for the image
+        String name = editText.getText().toString().trim();
+
+        //getting the actual path of the image
+        String path = getPath(filePath);
+        Log.d("PATH",path+"");
+
+        //Uploading code
+        try {
+            String uploadId = UUID.randomUUID().toString();
+
+            //Creating a multi part request
+            new MultipartUploadRequest(this, uploadId, Constants.UPLOAD_URL)
+                    .addFileToUpload(path, "image") //Adding file
+                    .addParameter("name", name) //Adding text parameter to the request
+                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload(); //Starting the upload
+
+        } catch (Exception exc) {
+            Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //method to show file chooser
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    private void showcamera() {
+        try {
+            startActivityForResult(cameraPhoto.takePhotoIntent(),REQUEST_CAMERA);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        cameraPhoto.addToGallery();
+    }
+
+    //handling the image chooser activity result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                Log.d("Bitmap ",""+filePath);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK ){
+            showFileChooser();
+        }
+    }
+
+    //method to get the file path from uri
+    public String getPath(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
+
+    //Requesting permission
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
+
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Displaying a toast
+                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    @Override
+    public void onClick(View v) {
+
+        if (v == buttonChoose) {
+            showFileChooser();
+        }
+        if (v == buttonUpload) {
+            try {
+                uploadMultipart();
+            }catch (NullPointerException e){
+                AlertDialog.Builder alerdialog = new AlertDialog.Builder(this);
+                alerdialog.setTitle("กรุณาเลือกภาพ");
+                alerdialog.setMessage("เลือกภาพหรือใช้กล้องถ่ายรูป");
+                alerdialog.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                alerdialog.show();
+            }
+
+        }
+        if (v == btnCamera){
+            showcamera();
+        }
+
+    }
+
+}
